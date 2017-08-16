8958a6c5d9a16af870a12d1d213d4c8b7cc38807
Carlos Mendes <carls.mendes@gmail.com>
2017-08-16 10:16:38 +0100

    google maps working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require_tree .
+
+// the code that you add here application.js run on ALL pages!
+// alert('Welcome to Carlos Flat!!!');
